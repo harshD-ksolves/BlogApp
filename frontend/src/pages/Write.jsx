823809715c19510b42ecb473c66eac27d7256956
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-10-17 19:08:25 +0530

    Added Create Blog ui

--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -5,15 +5,29 @@ import closeIcon from '../imgs/close.png';
 
 
 const Write = () => {
+  const [title,setTitle]=useState("");
   const [value,setValue]=useState('');
+  const [tags,setTags]=useState([]);
+  const [tag,setTag]=useState("");
 
+  const AddTag=(e)=>{
+    e.preventDefault();
+    if(tag){
+      setTags(tags=>[...tags,tag]);
+      setTag("");
+    }
+  }
+  const removeTag=(i)=>{
+    
+    setTags(tags=>tags.filter((tag,index)=> index!==i))
+    
+  }
 
-  console.log(value);
 
   return (
     <div className="create">
       <div className="content">
-        <input type="text" placeholder='Title'/>
+        <input type="text" placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
         <div className="writerContainer">
           <ReactQuill className="writer" theme='snow' value={value} onChange={setValue}/>
         </div>
@@ -37,14 +51,18 @@ const Write = () => {
         <div className="item">
           <h1>Tags</h1>
           <span className='tagInputContainer'>
-            <input type="text" id="tags"/>
-            <button>Add</button>
+            <input type="text" id="tags" onChange={(e)=>setTag(e.target.value)} value={tag}/>
+            <button onClick={(e)=>AddTag(e)}>Add</button>
           </span>
           <div className="tags">
-            <span className="tag">
-              <span>Hello There</span>
-              <img src={closeIcon} alt="" />
-            </span>
+            {
+              tags && tags.map((tag,index) => (
+                <span className="tag" key={index}>
+                  <span>{tag}</span>
+                  <img src={closeIcon} alt="" onClick={()=>removeTag(index)} />
+                </span>
+              ))
+            }
           </div>
         </div>
       </div>
