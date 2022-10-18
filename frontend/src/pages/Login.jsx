0cb0831ed2b9127113d3c21116bc345a13839d31
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-10-18 18:03:58 +0530

    added  login and register functionality with useState

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 const Login = () => {
+  const initial={username:'',password:''}
+  const [inputs,setInputs]=useState(initial);
+  const [error,setError]=useState("");
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const validateInputs=()=>{  
+
+    if( inputs.username.length <=3 ){
+      console.log(inputs.username.length)
+      setError("Provide valid username!");
+      return false;
+    }
+    
+    if(!inputs.password){
+      setError("Provide password!");
+      return false;
+    }
+    
+    setError("");
+    return true;
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+      if(!validateInputs()){
+        return;
+      }
+      
+      console.log(inputs)
+      setInputs(initial);
+  }
+
   return (
     <div className="auth">
       <h1>Login</h1>
@@ -12,17 +48,27 @@ const Login = () => {
           type="text"
           placeholder="username"
           name="username"
+          value={inputs.username}
+          onChange={(e)=>handleChange(e)}
         />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
+          value={inputs.password}
+          onChange={(e)=>handleChange(e)}
         />
-        <button>Login</button>
-        <p>This is an error!</p>
-        <span>Don't have account?  
-          <Link to="/register">Signup</Link> 
+        <button onClick={(e)=>handleSubmit(e)}>Login</button>
+        {error && <p className='error'>{error}</p>}
+        <span>Don't have account?
+          <Link 
+            to="/register" 
+            className='link' 
+            style={{ fontWeight: '600', fontSize: '13px', padding: '5px', color: '#ff0357' }}>
+
+            Signup
+          </Link>
         </  span>
       </form>
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+
+
+
 const Register = () => {
+
+  const initialState = {
+    username:"",
+    email:"",
+    password:"",
+  }
+
+  const [inputs,setInputs]=useState(initialState);
+  const [error,setError]=useState("");
+  const [success,setSuccess]=useState("");
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const validateInputs=()=>{  
+    if( inputs.username.length <=3 ){
+      console.log(inputs.username.length)
+      setError("Provide valid username!");
+      return false;
+    }
+    if(!inputs.email){
+      console.log(inputs.email.length);
+      setError("Provide valid email address!");
+      return false;
+    }
+    if(!inputs.password){
+      setError("Provide password!");
+      return false;
+    }
+    
+    setError("");
+    return true;
+  }
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+    if(!validateInputs()){
+      return;
+    }
+    setSuccess("User Registered Successfully!");
+    console.log(inputs)
+    setInputs(initialState);
+}
+
   return (
     <div className="auth">
       <h1>Register</h1>
@@ -10,23 +59,30 @@ const Register = () => {
           type="text"
           placeholder="username"
           name="username"
+          value={inputs.username}
+          onChange={(e)=>handleChange(e)}
         />
         <input
           required
           type="email"
           placeholder="abcd@gmail.com"
           name="email"
+          value={inputs.email}
+          onChange={(e)=>handleChange(e)}
         />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
+          value={inputs.password}
+          onChange={(e)=>handleChange(e)}
         />
-        <button>Login</button>
-        <p>This is an error!</p>
+        <button onClick={(e)=>handleSubmit(e)} >Login</button>
+        {error && <p className="error">{error}</p>}
+        {success && <p className="success">{success}</p>}
         <span>Already have an account?  
-          <Link to="/Login">Signin</Link> 
+          <Link to="/Login" className="link" style={{fontWeight:'600',fontSize:'13px',padding:'10px',color:'#3a5ce3'}}>Signin</Link> 
         </  span>
       </form>
     </div>
