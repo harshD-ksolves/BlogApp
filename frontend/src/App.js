10c47bcf9e806aeb8c16c4b85d5be1fe62c6ea8e
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-10-18 17:59:36 +0530

    organised Routes with Private and admin Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
+import React, { Suspense, lazy } from 'react';
+
 import {
-  createBrowserRouter,
-  RouterProvider,
+  Routes,
   Route,
   Outlet,
 } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Write from "./pages/Write";
-import Home from "./pages/Home";
-import Single from "./pages/Single";
+import PrivateRoute from './utils/PrivateRoute';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./style.scss"
+
+
+
+
+
+const Login=lazy(()=>import('./pages/Login'));
+const Register=lazy(()=>import('./pages/Register'));
+const Home = lazy(()=>import('./pages/Home'));
+const Single=lazy(()=>import('./pages/Single'));
+const Write=lazy(()=>import('./pages/Write'));
 
 const Layout = () => {
   return (
@@ -23,40 +30,23 @@ const Layout = () => {
   );
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/post/:id",
-        element: <Single />,
-      },
-      {
-        path: "/write",
-        element: <Write />,
-      },
-    ],
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+
 
 function App() {
   return (
     <div className="app">
       <div className="container">
-        <RouterProvider router={router} />
+      <Suspense fallback={<div>Loading.....</div>}>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route exact path ="/" element={<Home/>}/>
+            <Route path ="/post/:id" element={<Single/>}/>
+            <Route path ="/write" element={<Write/>}/>
+          </Route>
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Suspense>
       </div>
     </div>
   );
